7306f919e442a70126e3ae11bff5dcd368e6f347
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-04 16:07:37 +0300

    Aged Brie

--- test/aged-brie.spec.ts
+++ test/aged-brie.spec.ts
@@ -5,13 +5,13 @@ describe('Aged Brie', function () {
 
     const aged_brie = new GildedRose([ new Item('Aged Brie', 8, 40) ]);
 
-    it('Day One', function() {
+    it('One day passes - Brie quality increases and sellIn decreases', function() {
         const updated_aged_brie = aged_brie.updateQuality();
         expect(updated_aged_brie[0].quality).to.equal(41);
         expect(updated_aged_brie[0].sellIn).to.equal(7);
     });
 
-    it('Day Five', function() {
+    it('Five days pass - Brie quality increases and sellIn decreases', function() {
         const updated_aged_brie = aged_brie.updateQuality();
         aged_brie.updateQuality();
         aged_brie.updateQuality();
@@ -20,7 +20,7 @@ describe('Aged Brie', function () {
         expect(updated_aged_brie[0].sellIn).to.equal(3);
     });
 
-    it('Day Eight', function() {
+    it('Eight days pass - Brie quality increases and sellIn decreases', function() {
         const updated_aged_brie = aged_brie.updateQuality();
         aged_brie.updateQuality();
         aged_brie.updateQuality();
@@ -28,7 +28,7 @@ describe('Aged Brie', function () {
         expect(updated_aged_brie[0].sellIn).to.equal(0);
     });
 
-    it('Day Eleven', function() {
+    it('Eleven days pass - Brie quality increases up to 50 and then stops', function() {
         const updated_aged_brie = aged_brie.updateQuality();
         aged_brie.updateQuality();
         aged_brie.updateQuality();
