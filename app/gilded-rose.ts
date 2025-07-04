5c67141191e7a746d7307b51545e295f5197bf8c
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-04 16:33:57 +0300

    Updated Aged Brie

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,6 +23,10 @@ export class GildedRose {
                 if(this.items[i].name === 'Aged Brie'){
                     if (this.items[i].quality < 50) {
                         this.items[i].quality = this.items[i].quality + 1
+                        if (this.items[i].sellIn <= 0)
+                            this.items[i].quality = this.items[i].quality + 1
+                        if(this.items[i].quality > 50)
+                            this.items[i].quality = 50;
                     }
                 }
                 else{
--- test/aged-brie.spec.ts
+++ test/aged-brie.spec.ts
@@ -3,11 +3,11 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Aged Brie', function () {
 
-    const aged_brie = new GildedRose([ new Item('Aged Brie', 8, 40) ]);
+    const aged_brie = new GildedRose([ new Item('Aged Brie', 8, 35) ]);
 
     it('One day passes - Brie quality increases and sellIn decreases', function() {
         const updated_aged_brie = aged_brie.updateQuality();
-        expect(updated_aged_brie[0].quality).to.equal(41);
+        expect(updated_aged_brie[0].quality).to.equal(36);
         expect(updated_aged_brie[0].sellIn).to.equal(7);
     });
 
@@ -16,7 +16,7 @@ describe('Aged Brie', function () {
         aged_brie.updateQuality();
         aged_brie.updateQuality();
         aged_brie.updateQuality();
-        expect(updated_aged_brie[0].quality).to.equal(45);
+        expect(updated_aged_brie[0].quality).to.equal(40);
         expect(updated_aged_brie[0].sellIn).to.equal(3);
     });
 
@@ -24,13 +24,19 @@ describe('Aged Brie', function () {
         const updated_aged_brie = aged_brie.updateQuality();
         aged_brie.updateQuality();
         aged_brie.updateQuality();
-        expect(updated_aged_brie[0].quality).to.equal(48);
+        expect(updated_aged_brie[0].quality).to.equal(43);
         expect(updated_aged_brie[0].sellIn).to.equal(0);
     });
 
-    it('Eleven days pass - Brie quality increases up to 50 and then stops', function() {
+    it('Eleven days pass - Brie quality increases up to times faster because sell by date passed', function() {
         const updated_aged_brie = aged_brie.updateQuality();
         aged_brie.updateQuality();
+        aged_brie.updateQuality();
+        expect(updated_aged_brie[0].quality).to.equal(49);
+    });
+
+    it('Thirteen days pass - Brie quality increases up to 50 and then stops', function() {
+        const updated_aged_brie = aged_brie.updateQuality();
         aged_brie.updateQuality();
         expect(updated_aged_brie[0].quality).to.equal(50);
     });
