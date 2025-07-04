e9e8a27155e0d12bf3b3e263d83fb28fe96a1d28
Ana-Maria Predescu <Ana-Maria.Predescu@softwire.com>
2025-07-04 13:02:16 +0300

    Updated Gilded Rose

--- test/golden-master-text-test.spec.ts
+++ test/golden-master-text-test.spec.ts
@@ -3,46 +3,59 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Golden Master Test', function () {
     const item = new GildedRose([
-        new Item('Aged Brie', 10, 20) ,
+        new Item('Aged Brie', 10, 45) ,
         new Item('Sulfuras, Hand of Ragnaros', 1, 80),
-        new Item('Backstage passes to a TAFKAL80ETC concert', 6, 20) ,
-        new Item('normal item', 10, 20) ,
+        new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20) ,
+        new Item('normal item', 7, 20) ,
     ]);
 
     const updated_item = item.updateQuality();
 
     it('Day One', function() {
-        expect(updated_item[0].quality).to.equal(21);
+        expect(updated_item[0].quality).to.equal(46);
         expect(updated_item[1].quality).to.equal(80);
-        expect(updated_item[2].quality).to.equal(22);
+        expect(updated_item[2].quality).to.equal(21);
         expect(updated_item[3].quality).to.equal(19);
     });
 
     it('Day Two', function() {
-        const updated_item = item.updateQuality();
-        expect(updated_item[0].quality).to.equal(22);
+        item.updateQuality();
+        expect(updated_item[0].quality).to.equal(47);
         expect(updated_item[1].quality).to.equal(80);
-        expect(updated_item[2].quality).to.equal(25);
+        expect(updated_item[2].quality).to.equal(23);
         expect(updated_item[3].quality).to.equal(18);
     });
 
     it('Day Three', function() {
-        const updated_item = item.updateQuality();
-        expect(updated_item[0].quality).to.equal(23);
+        item.updateQuality();
+        expect(updated_item[0].quality).to.equal(48);
         expect(updated_item[1].quality).to.equal(80);
-        expect(updated_item[2].quality).to.equal(28);
+        expect(updated_item[2].quality).to.equal(25);
         expect(updated_item[3].quality).to.equal(17);
     });
 
-    it('Day Six', function() {
+    it('Day Five', function() {
         item.updateQuality();
         item.updateQuality();
         item.updateQuality();
-        const updated_item = item.updateQuality();
-        expect(updated_item[0].quality).to.equal(27);
+        item.updateQuality();
+        expect(updated_item[0].quality).to.equal(50);
+        expect(updated_item[1].quality).to.equal(80);
+        expect(updated_item[2].quality).to.equal(34);
+        expect(updated_item[3].quality).to.equal(13);
+    });
+
+    it('Day Eleven', function() {
+        item.updateQuality();
+        item.updateQuality();
+        item.updateQuality();
+        item.updateQuality();
+        item.updateQuality();
+        item.updateQuality();
+        expect(updated_item[0].quality).to.equal(50);
         expect(updated_item[1].quality).to.equal(80);
         expect(updated_item[2].quality).to.equal(0);
-        expect(updated_item[3].quality).to.equal(13);
+        expect(updated_item[3].quality).to.equal(1);
     });
 
 });
